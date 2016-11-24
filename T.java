0632c5736e236971eeea9d90c38691977e270222
dmpingfan <2964302469@qq.com>
2016-11-24 15:10:53 +0800

    Jni 关于数组传递的几种形式 完成版1.0

--- T.java
+++ T.java
@@ -3,16 +3,30 @@ public class T{
 	static{
 		System.loadLibrary("T");
 	}
-	public native String sendMessage();
+	public native String sendMessage(int rv);
 	
-	public native String changeMessage(String s);
+	public native String changeMessage(String s, int rv);
+	
+	public native String[] getMessage(int rv);
+	
+	public native String[] setMessage(String sendMessage1, String sendMessage2, int rv);
+	
+	public native String[] checkMessage(String[] sMessage, int rv);
+	
+	public native void cleanMessage(String[] sMessage, int rv);
 	
 	public static void main(String[] args){
-		String mMessage="Start";
-		String mResult="";
-		System.out.println("before change "+mMessage);
-		mResult=new T().changeMessage(mMessage);
-		System.out.println("Result is"+mResult);
-		System.out.println("after change "+mMessage);
+		int rv=0;
+		String sendMessage1="heihei";
+		String sendMessage2="haha";
+		String[] strList=new T().setMessage(sendMessage1,sendMessage2,rv);
+		for(int i=0; i<strList.length; i++){
+			System.out.println(strList[i]+" "+rv);
+		}
+		String[] strReturn=new T().checkMessage(strList,rv);
+		for(int i=0; i<strReturn.length; i++){
+			System.out.println(strReturn[i]+" "+rv);
+		}
+		new T().cleanMessage(strReturn,rv);
 	}
 }
